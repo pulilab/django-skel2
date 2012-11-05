c7ec7b1f37a11d14bfd812f7525c95269d4bca68
Viktor Nagy <viktor.nagy@gmail.com>
2012-11-05 14:58:45 +0100

    added raven (sentry) logging to production

--- project_name/settings.py
+++ project_name/settings.py
@@ -1,1 +1,33 @@
 from settings_common import *
+
+INSTALLED_APPS = INSTALLED_APPS + (
+    'raven.contrib.django',
+)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'WARNING',
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'sentry': {
+            'level': 'WARNING',
+            'class': 'raven.contrib.django.handlers.SentryHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'WARNING',
+            'propagate': True,
+        },
+    }
+}
+
+SENTRY_DSN = 'http://user:password@ip.address:port'
